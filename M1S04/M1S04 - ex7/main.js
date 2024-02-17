a1e291c2315588cb7ea4cb8d66dfd64202d0ad13
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-02-17 15:56:52 -0300

    Melhorando a lógica

--- M1S04/M1S04 - ex7/main.js
+++ M1S04/M1S04 - ex7/main.js
@@ -1,14 +1,27 @@
 // [M1S04] - Ex 7 - Função cumprimentos
 // Crie uma função que receba o valor de uma hora qualquer, e exibe no console uma mensagem de “bom dia”, “boa tarde” ou “boa noite”, de acordo com a hora passada.
 
-function greetings(hour) {
-  if (hour < 12) {
+let getHour = document.getElementById("hour");
+const greetingsText = document.getElementById("greetings-text");
+
+function greetings() {
+  let getValue = getHour.value;
+  let hour = getValue.split(":")[0];
+
+  if (hour < 12 && hour >= 5) {
+    greetingsText.textContent = "Bom dia! \u2615";
     console.log("Bom dia! \u2615");
   } else if (hour >= 12 && hour < 18) {
-    console.log("boa tarde! \u{1F31E}");
+    greetingsText.textContent = "Boa tarde! \u{1F31E}";
+    console.log("Boa tarde! \u{1F31E}");
   } else {
-    console.log("boa noite! \u{1F319}");
+    greetingsText.textContent = "Boa noite! \u{1F319}";
+    console.log("Boa noite! \u{1F319}");
   }
 }
 
-greetings(19);
+getHour.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
+    greetings();
+  }
+});
